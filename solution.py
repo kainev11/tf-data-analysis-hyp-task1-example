6604bd1e1be887efb2307e8a9eab4b130c400b94
Evgeny Nikolaev <50527841+kainev11@users.noreply.github.com>
2023-04-11 20:06:03 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,20 @@
-import pandas as pd
-import numpy as np
+from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 294776608
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    x_conv_rate = x_success / x_cnt
+    y_conv_rate = y_success / y_cnt
+    
+    std_error = np.sqrt(x_conv_rate * (1 - x_conv_rate) / x_cnt + y_conv_rate * (1 - y_conv_rate) / y_cnt)
+    
+    z_score = (y_conv_rate - x_conv_rate) / std_error
+    
+    alpha = 0.03
+    critical_value = norm.ppf(1 - alpha / 2)
+    
+    return z_score > critical_value
